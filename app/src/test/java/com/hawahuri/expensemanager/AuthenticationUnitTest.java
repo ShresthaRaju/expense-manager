c5708f26be7a11b89f55c80cc9b075f8056aee9e
ShresthaRaju <rajshres07@gmail.com>
2019-09-17 13:15:26 +0545

    Completed user authentication

--- app/src/test/java/com/hawahuri/expensemanager/AuthenticationUnitTest.java
+++ app/src/test/java/com/hawahuri/expensemanager/AuthenticationUnitTest.java
@@ -1,61 +1,77 @@
 package com.hawahuri.expensemanager;
 
+import com.hawahuri.expensemanager.impl.AuthImpl;
+import com.hawahuri.expensemanager.models.User;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertNull;
+import static junit.framework.TestCase.assertTrue;
 
-public class LoginRegisterTest {
+public class AuthenticationUnitTest {
+    private AuthImpl authImpl;
 
-    //Login Test
+    @Before
+    public void setup() {
+        authImpl = new AuthImpl();
+    }
+
+    // Sign Up Test
     @Test
-    public void testLoginUser() {
-        LoginBLL loginBLL = new LoginBLL("ujjwal", "ujjwalnjr11");
-        boolean result = loginBLL.checkLogin();
-        assertEquals(true, result);
+    public void testSignUp_ValidDetails_ShouldCreateNewUser() {
+        User user = new User("test", "test", "test@example.com", "password");
+        boolean signedUp = authImpl.registerUser(user);
+        assertTrue(signedUp);
     }
 
     @Test
-    public void testLoginFail() {
-        LoginBLL loginBLL = new LoginBLL("ufdajfa", "fkakfjka");
-        boolean result = loginBLL.checkLogin();
-        assertEquals(false, result);
+    public void testSignUp_EmptyField_ShouldNotCreateNewUser() {
+        User user = new User("", "test", "test@example.com", "password");
+        boolean signedUp = authImpl.registerUser(user);
+        assertFalse(signedUp);
     }
 
     @Test
-    public void testEmptyLogin() {
-        LoginBLL loginBLL = new LoginBLL("", "");
-        boolean result = loginBLL.checkLogin();
-        assertEquals(false, result);
-    }
-
-
-    //Register Test
-    @Test
-    public void testRegister() {
-        RegisterBLL registerBLL = new RegisterBLL("test", "test", "test@eg.com", "12345");
-        boolean result = registerBLL.checkRegister();
-        assertEquals(true, result);
+    public void testSignUp_InvalidDetails_ShouldNotCreateNewUser() {
+        User user = new User("t", "test", "test@example.com", "password");
+        boolean signedUp = authImpl.registerUser(user);
+        assertFalse(signedUp);
     }
 
     @Test
-    public void testInvalidRegister() {
-        RegisterBLL registerBLL = new RegisterBLL("test ", "test", "test.com", "12345");
-        boolean result = registerBLL.checkRegister();
-        assertEquals(false, result);
+    public void testSignUp_ExistingEmail_ShouldNotCreateNewUser() {
+        User user = new User("test", "test", "test@example.com", "password");
+        boolean signedUp = authImpl.registerUser(user);
+        assertFalse(signedUp);
+    }
+
+    // Sign In Test
+
+    @Test
+    public void testLogin_ValidDetails_ShouldReturnAuthUser() {
+
+        User authUser = authImpl.loginUser("test@example.com", "password");
+
+        assertEquals("test@example.com", authUser.getEmail());
     }
 
     @Test
-    public void testEmptyRegister() {
-        RegisterBLL registerBLL = new RegisterBLL(" ", "test", "test.com", "12345");
-        boolean result = registerBLL.checkRegister();
-        assertEquals(false, result);
+    public void testLogin_EmptyField_ShouldDenyLogin() {
+
+        User authUser = authImpl.loginUser(" ", "password");
+
+        assertNull(authUser);
     }
 
     @Test
-    public void testExistingUser() {
-        RegisterBLL registerBLL = new RegisterBLL("test", "test", "test@eg.com", "12345");
-        boolean result = registerBLL.checkRegister();
-        assertEquals(false, result);
+    public void testLogin_InvalidDetails_ShouldDenyLogin() {
+
+        User authUser = authImpl.loginUser("rest@example.com", "password");
+
+        assertNull(authUser);
     }
 
 }
