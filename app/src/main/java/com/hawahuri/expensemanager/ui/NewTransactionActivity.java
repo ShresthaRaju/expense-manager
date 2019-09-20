d4b35af2b6c8f6ba98ce46d92261879264ef8f25
bishes <bishesu@gmail.com>
2019-09-20 16:17:25 +0545

    Added new transaction activity

---
+++ app/src/main/java/com/hawahuri/expensemanager/ui/NewTransactionActivity.java
@@ -0,0 +1,16 @@
+package com.hawahuri.expensemanager.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hawahuri.expensemanager.R;
+
+public class NewTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_transaction);
+    }
+}
